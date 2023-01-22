670a5cdc954da59cf0d4ad3b929197c997d89809
Danilov Eugeniu <109844498+svoliard@users.noreply.github.com>
2023-01-22 13:00:42 +0300

    uploadSecondTry

--- myfirstproject_CV/js/script.js
+++ myfirstproject_CV/js/script.js
@@ -1,7 +1,8 @@
 function refreshTime() {
-    const timeDisplay = document.getElementById("time");
-    const dateString = new Date().toLocaleString();
-    const formattedString = dateString.replace(", ", " - ");
-    timeDisplay.textContent = formattedString;
-  }
-    setInterval(refreshTime, 1000);
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+setInterval(refreshTime, 1000);
+
